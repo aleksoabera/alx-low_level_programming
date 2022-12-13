4540051daf0a8037d96a9672af310bf8cbab43d0
aleksoabera <aleksoabera@gmail.com>
2022-12-13 10:21:51 -0800

    ok

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,10 +3,18 @@
  * Description: Prints positive, negative or zero
  * Return: 0
  */
+#include "main.h"
 #include <stdio.h>
 int main(void)
 {
-  printf("_putchar");
-  putchar ('\n');
+ _ putchar ('_');
+ _ putchar ('p');
+ _ putchar ('u');
+ _ putchar ('t');
+ _ putchar ('c');
+ _ putchar ('h');
+  _ putchar ('a');
+  _ putchar ('r');
+  _ putchar ('\n');
   return (0);
 }
