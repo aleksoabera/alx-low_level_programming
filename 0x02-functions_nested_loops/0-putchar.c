81adde9a362f07bae91435f5c8861c2b448fd905
aleksoabera <aleksoabera@gmail.com>
2022-12-14 07:29:38 -0800

    well done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+/**
+ * main - prints to string
+ * Description: _putchar followed by new line
+ * Return: 0
+ */
+#include <stdio.h>
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar ('\n');
+return (0);
+}
