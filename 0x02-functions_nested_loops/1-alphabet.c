ac1b19b1f4e201797b1fba71b2648ef0d93c6750
aleksoabera <aleksoabera@gmail.com>
2022-12-14 07:55:33 -0800

    prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,8 +4,8 @@
  * description print_alphabet
  * Return: Always 0.
  */
-void print_alphabet(void);
-{
+void print_alphabet(void){
+
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
@@ -13,3 +13,4 @@ _putchar(ch);
 }
 _putchar('\n');
 }
+
